d0df3b49c399db5c3f92e61ed22e0afb3b9fc244
NilsonKr <nuotrok444@gmail.com>
2022-05-19 21:27:28 -0500

    :sparkles: Register component with web3
    - Handling loading and error states in register component
    - useForm hook to handle forms states
    - Update UI details and contract direction on rinkeby testnet

--- components/Form/Register.tsx
+++ components/Form/Register.tsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { authContext } from '@context/AuthContext'
+import { useForm } from '@hooks/useForm'
 //UI
 import {
 	InputGroup,
@@ -9,8 +10,13 @@ import {
 	Heading,
 	VStack,
 	Icon,
+	Alert,
+	AlertIcon,
+	AlertTitle,
+	Spinner,
+	useToast,
+	FormControl,
 } from '@chakra-ui/react';
-import { IoMdSend } from 'react-icons/io';
 //HOC
 import { ConnectAnimation } from '@components/HOC/ConnectAnimation'
 
@@ -20,45 +26,78 @@ type ComponenetProps = {
 
 export const Register: React.FC<ComponenetProps> = ConnectAnimation(({ msg }) => {
 	const { register } = useContext(authContext)
+	const toast = useToast()
+	const { form, handleChange } = useForm({ name: '' })
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const [error, setError] = useState<string>(null)
+
+	const handleRegister = async (ev: React.FormEvent<HTMLFormElement>) => {
+		ev.preventDefault()
+
+		setIsLoading(true)
+		toast({
+			title: 'Please wait until confirmation',
+			description: 'This process could take a couple of minutes once you sign the transaction, Thanks!',
+			variant: 'solid',
+			position: 'top-left',
+			isClosable: true,
+			duration: 20000,
+		})
+		const result = await register(form.name as string)
+
+		if (result.error) {
+			setError(result.error)
+		}
+
+		console.log(result, 'payload')
+		setIsLoading(false)
+	}
 
 	return (
-		<VStack spacing={5} align='center'>
+		<VStack spacing={5} align='center' position='relative'>
+			{(msg || error) && <Alert status={error ? 'error' : 'warning'} position='absolute' top='-40px'>
+				<AlertIcon />
+				<AlertTitle>{error ? error : msg}</AlertTitle>
+			</Alert>}
+
 			<Heading>Welcome! Please set your name✨</Heading>
-			<InputGroup
-				w='75%'
-				m='0 auto'
-				borderColor='transparent'
-				borderBottom='1px solid white'
-			>
-				<Input
-					_focus={{ borderColor: 'transparent' }}
-					border='none'
-					placeholder='Type your username'
-					_placeholder={{ color: 'gray.400' }}
-				/>
-				<InputRightElement>
-					<Icon
-						color='purple.500'
-						cursor='pointer'
-						h='22px'
-						w='25px'
-						_hover={{ color: 'purple.300' }}
-						as={IoMdSend}
-					/>
-				</InputRightElement>
-			</InputGroup>
-			<Button
-				_hover={{ bg: 'purple.400' }}
-				_active={{ bg: 'purple.800', transform: 'scale(0.95)' }}
-				bg='purple.600'
-				px='25px'
-				py='20px'
-				borderRadius='25px'
-				fontSize='1xl'
-				onClick={() => { }}
-			>
-				Create account
-			</Button>
+			<form onSubmit={handleRegister} style={{ width: '75%', margin: '0 auto' }}>
+				<FormControl isInvalid={!!error} mt='4' display='flex' flexDirection='column' alignItems='center' gap='4'>
+					<InputGroup
+						borderColor='transparent'
+						borderBottom={`1px solid ${error ? '#ff4d6c' : 'white'}`}
+					>
+						<Input
+							_invalid={{
+								borderColor: 'transparent',
+							}}
+							_focus={{ borderColor: 'transparent' }}
+							border='none'
+							placeholder='Type your username'
+							_placeholder={{ color: 'gray.400' }}
+							value={form.name}
+							onChange={(ev) => handleChange('name', ev.target.value)}
+						/>
+						{isLoading && <InputRightElement p='2' >
+							<Spinner size='md' color='purple.500' />
+						</InputRightElement>}
+					</InputGroup>
+					<Button
+						type='submit'
+						_hover={{ bg: 'purple.400' }}
+						_active={{ bg: 'purple.800', transform: 'scale(0.95)' }}
+						bg='purple.600'
+						px='25px'
+						py='20px'
+						minW='200px'
+						borderRadius='25px'
+						fontSize='1xl'
+						disabled={form.name === ''}
+					>
+						{isLoading ? <Spinner size='md' color='white' /> : 'Create account'}
+					</Button>
+				</FormControl>
+			</form>
 		</VStack>
 	);
 });
